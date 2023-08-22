a42750dddcb820b180e7bc5b324a90af9f8b9338
Mohammad-Javad-Ameri <110919726+Mohammad-Javad-Ameri@users.noreply.github.com>
2023-08-22 19:21:54 +0330

    add comments
    forget to save then commit this comments

--- todo-list/__tests__/components/Header.test.js
+++ todo-list/__tests__/components/Header.test.js
@@ -3,25 +3,32 @@ import { render, fireEvent, screen } from "@/utils/test-utils";
 import { useSelector } from "react-redux";
 import Header from "@/components/Header";
 
+// Initial state for the redux store
 const initialState = {
   theme: {
     theme: "light",
   },
 };
 
+// Test suite for Header component
 describe("Header", () => {
+  // Test case for correct rendering of Header
   it("should render correctly", () => {
+    // Rendering the Header component with initial state
     const { container } = render(<Header />, { initialState });
+    // Checking if the rendered container is present in the document
     expect(container).toBeInTheDocument();
   });
 
+  // Test case for theme change on toggle button click
   it("should change the theme when the toggle button is clicked", () => {
     // Mock component that renders the current theme
     const CurrentTheme = () => {
+      // Using useSelector hook to get the current theme from the state
       const theme = useSelector((state) => state.theme.theme);
       return <div>{theme}</div>;
     };
-
+    // Rendering the Header and CurrentTheme components with initial state
     render(
       <>
         <Header />
@@ -30,7 +37,9 @@ describe("Header", () => {
       { initialState }
     );
 
+    // Getting the toggle button by its role
     const toggleButton = screen.getByRole("button");
+    // Simulating a click event on the toggle button
     fireEvent.click(toggleButton);
 
     // Check if the theme in the state has changed
